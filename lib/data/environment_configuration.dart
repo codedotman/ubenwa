482610e59aadaab426cea3be8782e424ea418ef5
codedotman <dotunaboyeji@gmail.com>
2022-06-19 21:25:52 +0100

    Update environment_configuration.dart

--- lib/data/environment_configuration.dart
+++ lib/data/environment_configuration.dart
@@ -1,6 +1,6 @@
 class EnvironmentConfiguration {
   static const BASE_API_URL = String.fromEnvironment('BASE_API_URL',
-      defaultValue: 'https://todlr-v1-prod.herokuapp.com/v1');
+      defaultValue: '');
 
   static const IS_PRODUCTION =
       bool.fromEnvironment('dart.vm.product', defaultValue: false);
